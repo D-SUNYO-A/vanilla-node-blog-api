7cd6eada40e3cb42b410be591af772e1b1891032
D-SUNYO-A <sdanarson1@gmail.com>
2024-01-13 15:02:23 +0300

    version-2

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 import http from "http";
 import cluster from "cluster";
 import os from 'os';
+import cors from "cors";
 import { postController } from "./controllers/postController.js";
 import { responseUtil } from "./utils/responseUtil.js";
 import mongoose from "mongoose";
+
+const corsOptions = {
+    origin: "http://127.0.0.1:5500",
+    methods: "GET",
+};
 
 const uri = "mongodb+srv://sdanarson1:YF078se0zrRptXYn@cluster0.ebxzpgl.mongodb.net/blog?retryWrites=true&w=majority";
 
@@ -24,22 +30,24 @@ if (cluster.isPrimary) {
     });
 } else {
     const app = http.createServer((req, res) => {
-        if(req.url === '/api/posts' && req.method === 'POST') {
-            postController.createPost(req, res);
-        } else if(req.url === '/api/posts' && req.method === 'GET') {
-            postController.getPosts(req, res);  
-        } else if(req.url.match(/\/api\/post\/([0-9]+)/) && req.method === 'GET') {
-            const id = req.url.split('/')[3]; // api/post/1
-            postController.getPost(req, res, id);
-        } else if(req.url.match(/\/api\/post\/([0-9]+)/) && req.method === 'PUT') {
-            const id = req.url.split('/')[3]; // api/post/1
-            postController.updatePost(req, res, id);
-        } else if(req.url.match(/\/api\/post\/([0-9]+)/) && req.method === 'DELETE') {
-            const id = req.url.split('/')[3]; // api/post/1
-            postController.deletePost(req, res, id);
-        } else {
-            responseUtil.responseNotFound(res);
-        }
+        cors(corsOptions)(req, res, async () => {
+            if(req.url === '/api/posts' && req.method === 'POST') {
+                postController.createPost(req, res);
+            } else if(req.url === '/api/posts' && req.method === 'GET') {
+                postController.getPosts(req, res);  
+            } else if(req.url.match(/\/api\/post\/([0-9]+)/) && req.method === 'GET') {
+                const id = req.url.split('/')[3]; // api/post/1
+                postController.getPost(req, res, id);
+            } else if(req.url.match(/\/api\/post\/([0-9]+)/) && req.method === 'PUT') {
+                const id = req.url.split('/')[3]; // api/post/1
+                postController.updatePost(req, res, id);
+            } else if(req.url.match(/\/api\/post\/([0-9]+)/) && req.method === 'DELETE') {
+                const id = req.url.split('/')[3]; // api/post/1
+                postController.deletePost(req, res, id);
+            } else {
+                responseUtil.responseNotFound(res);
+            }
+        });
     });  
     
     mongoose
